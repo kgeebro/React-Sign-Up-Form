31c444d50a8297772136eb03c256ad516ce15da1
kgeebro <kgeebro@gmail.com>
2019-03-13 05:07:16 -0400

    this.handleChange added and componentMount methods commented out

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,14 +1,14 @@
-"use strict";
+'use strict';
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
+function _defineProperty(obj, key, value) { if (key in obj) { Object.defineProperty(obj, key, { value: value, enumerable: true, configurable: true, writable: true }); } else { obj[key] = value; } return obj; }
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-console.log("App Is Running!!!");
 
 var SignUpForm = function (_React$Component) {
     _inherits(SignUpForm, _React$Component);
@@ -18,80 +18,78 @@ var SignUpForm = function (_React$Component) {
 
         var _this = _possibleConstructorReturn(this, (SignUpForm.__proto__ || Object.getPrototypeOf(SignUpForm)).call(this, props));
 
+        _this.handleChange = _this.handleChange.bind(_this);
         _this.state = {
-            email: "Kgeebro@gmail.com",
-            firstName: "Kannah",
-            lastName: "TESTINGGGG"
+            email: undefined,
+            firstName: undefined,
+            lastName: undefined
         };
         return _this;
     }
 
-    // const user = {
-    //     email: "Test@gmail.com"
-    //     firstName: "kannah",
-    //     lastName: "geebro"
-    // }
-
-
-    // const addEmail = () => {
-    //     user.email = e.target.value;
-    // }
-
-    // const addFirst = () => {
-    //     e.preventDefault();
-    //     user.firstName = e.target.value;
-    // } 
-
-    // const addLast = () => {
-    //     user.lastName = e.target.value;
-    // }
-
-
     _createClass(SignUpForm, [{
-        key: "render",
+        key: 'componentDidMount',
+        value: function componentDidMount() {
+            localStorage.getItem('email');
+        }
+    }, {
+        key: 'componentDidUpdate',
+        value: function componentDidUpdate(prevProps, prevState) {
+            if (prevState.email !== this.state.email) {
+                localStorage.setItem('email', this.state.email);
+            }
+        }
+    }, {
+        key: 'handleSubmit',
+        value: function handleSubmit() {
+            event.preventDefault();
+        }
+    }, {
+        key: 'handleChange',
+        value: function handleChange(e) {
+            this.setState(_defineProperty({}, e.target.name, e.target.value));
+        }
+    }, {
+        key: 'render',
         value: function render() {
             return React.createElement(
-                "div",
+                'div',
                 null,
                 React.createElement(
-                    "h1",
+                    'h1',
                     null,
-                    "Welcome To FirstProject"
+                    'Welcome To FirstProject'
                 ),
                 React.createElement(
-                    "h3",
+                    'h3',
                     null,
-                    "Sign Up. It'll make me feel good"
+                    'Sign Up Today!!!'
                 ),
                 React.createElement(
-                    "form",
-                    null,
-                    React.createElement("input", { type: "email", placeholder: "Email" }),
-                    React.createElement("input", { type: "text", placeholder: "First" }),
-                    React.createElement("input", { type: "text", placeholder: "Last" }),
-                    React.createElement("input", { type: "submit", value: "Submit" })
+                    'form',
+                    { onSubmit: this.handleSubmit },
+                    React.createElement('input', { type: 'email', placeholder: 'Email', name: 'email', onChange: this.handleChange }),
+                    React.createElement('input', { type: 'text', placeholder: 'First', name: 'firstName', onChange: this.handleChange }),
+                    React.createElement('input', { type: 'text', placeholder: 'Last', name: 'lastName', onChange: this.handleChange }),
+                    React.createElement('input', { type: 'submit', value: 'Submit' })
                 ),
                 React.createElement(
-                    "ul",
+                    'p',
                     null,
-                    React.createElement(
-                        "li",
-                        null,
-                        "First Name: ",
-                        this.state.firstName
-                    ),
-                    React.createElement(
-                        "li",
-                        null,
-                        "Last Name: ",
-                        this.state.lastName
-                    ),
-                    React.createElement(
-                        "li",
-                        null,
-                        "Email: ",
-                        this.state.email
-                    )
+                    'First Name: ',
+                    this.state.firstName
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    'Last Name: ',
+                    this.state.lastName
+                ),
+                React.createElement(
+                    'p',
+                    null,
+                    'Email: ',
+                    this.state.email
                 )
             );
         }
@@ -103,3 +101,76 @@ var SignUpForm = function (_React$Component) {
 var appRoot = document.getElementById('app');
 
 ReactDOM.render(React.createElement(SignUpForm, null), appRoot);
+
+//JUST STYLE THE PAGE AND BUILD THE FILE STRUCTURE.
+//FUNCTIONALITY WILL COME LATER. THIS IS THE MOST BASIC PROJECT THAT YOU CAN FINISH.
+
+
+//___________________________________________________________________________________________________
+// class NameForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {value: ''};
+
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+
+//     handleChange(event) {
+//       this.setState({value: event.target.value});
+//     }
+
+//     handleSubmit(event) {
+//       alert('A name was submitted: ' + this.state.value);
+//       event.preventDefault();
+//     }
+
+//     render() {
+//       return (
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Name:
+//             <input type="text" value={this.state.value} onChange={this.handleChange} />
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//       );
+//     }
+//   }
+
+//   var app = document.getElementById('app');
+
+//   ReactDOM.render(<NameForm />, app);
+//___________________________________________________________________________________________________
+// class Login extends Component {
+
+//     constructor () {
+//       super();
+//       this.state = {
+//         email: '',
+//         password: ''
+//       };
+//       this.handleChange = this.handleChange.bind(this);
+//     }
+
+//     handleChange (evt) {
+//       // check it out: we get the evt.target.name (which will be either "email" or "password")
+//       // and use it to target the key on our `state` object with the same name, using bracket syntax
+//       this.setState({ [evt.target.name]: evt.target.value });
+//     }
+
+//     render () {
+//       return (
+//         <form>
+
+//           <label>Email</label>
+//           <input type="text" name="email" onChange={this.handleChange} />
+
+//           <label>Password</label>
+//           <input type="password" name="password" onChange={this.handleChange} />
+
+//         </form>
+//       );
+//     }
+//   }
+//___________________________________________________________________________________________________
--- src/app.js
+++ src/app.js
@@ -1,31 +1,39 @@
 class SignUpForm extends React.Component {
     constructor(props) {
         super(props);
+        this.handleChange = this.handleChange.bind(this);
         this.state = {
-            email: "Kgeebro@gmail.com",
-            firstName: "Kannah",
-            lastName: "Geebro"
+            email: undefined,
+            firstName: undefined,
+            lastName: undefined
         }
     }
 
+        handleSubmit(){
+            event.preventDefault();
+        }
 
+        handleChange(e){
+            this.setState({
+                    [e.target.name]: e.target.value
+            });
+        }
 
         render(){
             return(
                 <div>
                     <h1>Welcome To FirstProject</h1>
-                    <h3>Sign Up. It'll make me feel good</h3>
-                    <form>
-                        <input type="email" placeholder="Email"></input>
-                        <input type="text" placeholder="First"></input>
-                        <input type="text" placeholder="Last"></input>
+                    <h3>Sign Up Today!!!</h3>
+                    <form onSubmit={this.handleSubmit}>
+                        <input type="email" placeholder="Email" name="email" onChange={this.handleChange}/>
+                        <input type="text" placeholder="First" name="firstName" onChange={this.handleChange}/>
+                        <input type="text" placeholder="Last" name="lastName" onChange={this.handleChange}/>
                         <input type="submit" value="Submit"></input>
                     </form>
-                    <ul>
-                        <li>First Name: {this.state.firstName}</li>
-                        <li>Last Name: {this.state.lastName}</li>
-                        <li>Email: {this.state.email}</li>
-                    </ul>
+
+                    <p>First Name: {this.state.firstName}</p>
+                    <p>Last Name: {this.state.lastName}</p>
+                    <p>Email: {this.state.email}</p>
                 </div>
             );
         }
@@ -35,13 +43,98 @@ class SignUpForm extends React.Component {
 
 
 
-
-
-
 var appRoot = document.getElementById('app');
 
 
 ReactDOM.render(<SignUpForm />, appRoot);
 
+//JUST STYLE THE PAGE AND BUILD THE FILE STRUCTURE.
+//FUNCTIONALITY WILL COME LATER. THIS IS THE MOST BASIC PROJECT THAT YOU CAN FINISH.
 
 
+
+
+
+
+
+
+
+        // componentDidMount(){
+        //     localStorage.getItem('email');
+        // }
+
+        // componentDidUpdate(prevProps, prevState){
+        //     if(prevState.email !== this.state.email) {
+        //         localStorage.setItem('email', this.state.email);
+        //     }
+        // }
+
+
+//___________________________________________________________________________________________________
+// class NameForm extends React.Component {
+//     constructor(props) {
+//       super(props);
+//       this.state = {value: ''};
+  
+//       this.handleChange = this.handleChange.bind(this);
+//       this.handleSubmit = this.handleSubmit.bind(this);
+//     }
+  
+//     handleChange(event) {
+//       this.setState({value: event.target.value});
+//     }
+  
+//     handleSubmit(event) {
+//       alert('A name was submitted: ' + this.state.value);
+//       event.preventDefault();
+//     }
+  
+//     render() {
+//       return (
+//         <form onSubmit={this.handleSubmit}>
+//           <label>
+//             Name:
+//             <input type="text" value={this.state.value} onChange={this.handleChange} />
+//           </label>
+//           <input type="submit" value="Submit" />
+//         </form>
+//       );
+//     }
+//   }
+
+//   var app = document.getElementById('app');
+
+//   ReactDOM.render(<NameForm />, app);
+//___________________________________________________________________________________________________
+// class Login extends Component {
+
+//     constructor () {
+//       super();
+//       this.state = {
+//         email: '',
+//         password: ''
+//       };
+//       this.handleChange = this.handleChange.bind(this);
+//     }
+    
+//     handleChange (evt) {
+//       // check it out: we get the evt.target.name (which will be either "email" or "password")
+//       // and use it to target the key on our `state` object with the same name, using bracket syntax
+//       this.setState({ [evt.target.name]: evt.target.value });
+//     }
+    
+//     render () {
+//       return (
+//         <form>
+        
+//           <label>Email</label>
+//           <input type="text" name="email" onChange={this.handleChange} />
+          
+//           <label>Password</label>
+//           <input type="password" name="password" onChange={this.handleChange} />
+          
+//         </form>
+//       );
+//     }
+//   }
+//___________________________________________________________________________________________________
